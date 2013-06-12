f25daddb89e988c6a20bb56bccd36b659b3a7722
Austen Dicken <cvpcsm@gmail.com>
2013-06-12 13:39:02 -0500

    Added 'Inactive' state
    Events can be 'Inactive' if they are not in warmup
    but need to be explicitly activated by another event

--- GuildWars2.ArenaNet/Model/EventStateType.cs
+++ GuildWars2.ArenaNet/Model/EventStateType.cs
@@ -9,6 +9,7 @@ namespace GuildWars2.ArenaNet.Model
         Success,    // event succeeded
         Fail,       // event failed
         Warmup,     // event is inactive and will only become active once certain criteria are met
+        Inactive,   // event is not active until activated by some external event
         Preparation // event warmup criteria are met, but certain auto-running activities have not been completed yet to become active
     }
 }
