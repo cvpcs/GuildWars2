e655be0d10c996b4d0abc067f23070f6ce0c6e18
Austen Dicken <cvpcsm@gmail.com>
2013-06-29 02:01:54 -0500

    Use built-in brushes instead of static ones

--- GuildWars2.ArenaNet.Mapper/EventMapPolygon.cs
+++ GuildWars2.ArenaNet.Mapper/EventMapPolygon.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Media;
-using System.Windows.Shapes;
 
 using Microsoft.Maps.MapControl.WPF;
 using Location = Microsoft.Maps.MapControl.WPF.Location;
@@ -12,16 +11,13 @@ namespace GuildWars2.ArenaNet.Mapper
 {
     public class EventMapPolygon : MapPolygon
     {
-        private static SolidColorBrush FILL_BRUSH = new SolidColorBrush(Colors.Red);
-        private static SolidColorBrush STROKE_BRUSH = new SolidColorBrush(Colors.Maroon);
-
         public EventMapPolygon(EventDetails ev)
         {
             Locations = new LocationCollection();
             Opacity = 0.5;
             StrokeThickness = 2;
-            Fill = FILL_BRUSH;
-            Stroke = STROKE_BRUSH;
+            Fill = Brushes.Red;
+            Stroke = Brushes.Maroon;
         }
 
         public void SetEventState(EventStateType state)
