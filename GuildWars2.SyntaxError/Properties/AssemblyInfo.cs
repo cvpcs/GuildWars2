d5e18a4a9899859a467f886d558e4e97180abcfc
Austen Dicken <cvpcsm@gmail.com>
2013-07-01 16:50:50 -0500

    Update assembly name info for SE project

--- GuildWars2.SyntaxError/Properties/AssemblyInfo.cs
+++ GuildWars2.SyntaxError/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GuildWars2.SE")]
+[assembly: AssemblyTitle("GuildWars2.SyntaxError")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GuildWars2.SE")]
+[assembly: AssemblyProduct("GuildWars2.SyntaxError")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
