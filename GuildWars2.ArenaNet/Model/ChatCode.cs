0575383ef3ed2b455991c7e3f3231f4064a72b03
Austen Dicken <cvpcsm@gmail.com>
2013-07-01 13:13:20 -0500

    Added ChatCode logic

---
+++ GuildWars2.ArenaNet/Model/ChatCode.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GuildWars2.ArenaNet.Model
+{
+    public class ChatCode
+    {
+        public ChatCodeType Header
+        {
+            get
+            {
+                if (Data != null && Data.Length > 0 && Enum.IsDefined(typeof(ChatCodeType), Data[0]))
+                {
+                    return (ChatCodeType)Data[0];
+                }
+
+                return ChatCodeType.Invalid;
+            }
+        }
+
+        public byte[] Data { get; private set; }
+
+        private ChatCode(byte[] data)
+        {
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[&{0}]", Convert.ToBase64String(Data, Base64FormattingOptions.None));
+        }
+
+        #region Static Creation Methods
+        public static ChatCode CreateCoinLink(uint c, uint s = 0, uint g = 0) { return CreateIntCode(ChatCodeType.Coin, (g * 10000) + (s * 100) + c); }
+
+        public static ChatCode CreateItemLink(uint item_id, byte qty = 1)
+        {
+            byte[] tmp = CreateIntCode(ChatCodeType.Item, item_id).Data;
+
+            byte[] data = new byte[tmp.Length + 1];
+            data[0] = tmp[0];
+            data[1] = qty;
+
+            Array.Copy(tmp, 1, data, 2, tmp.Length - 1);
+
+            return new ChatCode(data);
+        }
+
+        public static ChatCode CreateTextString(uint string_id) { return CreateIntCode(ChatCodeType.Text, string_id); }
+        public static ChatCode CreateMapLink(uint poi_id) { return CreateIntCode(ChatCodeType.Map, poi_id); }
+        public static ChatCode CreateSkillLink(uint skill_id) { return CreateIntCode(ChatCodeType.Skill, skill_id); }
+        public static ChatCode CreateTraitLink(uint trait_id) { return CreateIntCode(ChatCodeType.Trait, trait_id); }
+        public static ChatCode CreateRecipeLink(uint recipe_id) { return CreateIntCode(ChatCodeType.Recipe, recipe_id); }
+
+        private static ChatCode CreateIntCode(ChatCodeType header, UInt32 id)
+        {
+            byte[] bytes = BitConverter.GetBytes(id);
+
+            if (!BitConverter.IsLittleEndian)
+                Array.Reverse(bytes);
+
+            byte[] data = new byte[bytes.Length + 1];
+
+            data[0] = (byte)header;
+            Array.Copy(bytes, 0, data, 1, bytes.Length);
+
+            return new ChatCode(data);
+        }
+        #endregion
+    }
+}
---
+++ GuildWars2.ArenaNet/Model/ChatCodeType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GuildWars2.ArenaNet.Model
+{
+    public enum ChatCodeType
+    {
+        Invalid = 0x00,
+        Coin = 0x01,
+        Item = 0x02,
+        Text = 0x03,
+        Map = 0x04,
+        Skill = 0x07,
+        Trait = 0x08,
+        Player = 0x09,
+        Recipe = 0x0A
+    }
+}
