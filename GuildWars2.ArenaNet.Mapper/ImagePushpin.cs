be7b353d912885c88241ed1a138c4b14ebb299bf
Austen Dicken <cvpcsm@gmail.com>
2013-08-20 08:51:29 -0500

    Offload the XAML template into a separate file
    For the sake of cleanliness, move the ImagePushpin template into
    its own file.

--- GuildWars2.ArenaNet.Mapper/ImagePushpin.cs
+++ GuildWars2.ArenaNet.Mapper/ImagePushpin.cs
@@ -1,19 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 using Microsoft.Maps.MapControl.WPF;
 
@@ -22,20 +11,8 @@ namespace GuildWars2.ArenaNet.Mapper
     public abstract class ImagePushpin : Pushpin
     {
         private static string IMAGEBRUSH_NAME = "ImagePushpinTemplateBrush";
-        private static ControlTemplate TEMPLATE = (ControlTemplate)XamlReader.Parse(string.Format(@"
-                <ControlTemplate xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
-                                 xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
-                                 xmlns:l=""clr-namespace:GuildWars2.ArenaNet.Mapper;assembly=GuildWars2.ArenaNet.Mapper""
-                                 xmlns:m=""clr-namespace:Microsoft.Maps.MapControl.WPF;assembly=Microsoft.Maps.MapControl.WPF"">
-                    <Rectangle Width=""{{Binding Path=Width, RelativeSource={{RelativeSource FindAncestor,AncestorType=m:Pushpin}}}}""
-                               Height=""{{Binding Path=Height, RelativeSource={{RelativeSource FindAncestor,AncestorType=m:Pushpin}}}}"">
-                        <Rectangle.Fill>
-                            <ImageBrush x:Name=""{0}""
-                                        ImageSource=""{{Binding Path=Image, RelativeSource={{RelativeSource FindAncestor,AncestorType=l:ImagePushpin}}}}"" />
-                        </Rectangle.Fill>
-                    </Rectangle>
-                </ControlTemplate>
-                ", IMAGEBRUSH_NAME));
+        private static ControlTemplate TEMPLATE = (ControlTemplate)Application.LoadComponent(
+                new Uri("/ImagePushpinTemplate.xaml", UriKind.Relative));
 
         private bool m_TemplateApplied = false;
 
