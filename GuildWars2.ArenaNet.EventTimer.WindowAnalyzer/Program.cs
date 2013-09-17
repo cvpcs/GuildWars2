5e4410df58415afc11dfdcb5fd5fa1c8d85d4119
Austen Dicken <cvpcsm@gmail.com>
2013-09-17 13:53:18 -0500

    Add some new projects
    GuildWars2.ArenaNet.EventTimer.WindowAnalyzer:
      tracks time events are down to help determine window times
    GuildWars2.ArenaNet.Mapper.JS
      javascript port of the mapper tool
    GuildWars2.TradingPost.Test
      test program for the non-public ArenaNet APIs

---
+++ GuildWars2.ArenaNet.EventTimer.WindowAnalyzer/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Timers;
+using System.Threading;
+using Timer = System.Timers.Timer;
+
+using GuildWars2.SyntaxError.API;
+using GuildWars2.SyntaxError.Model;
+
+namespace GuildWars2.ArenaNet.EventTimer.WindowAnalyzer
+{
+    public class Program
+    {
+        private static TimeSpan m_PollRate = new TimeSpan(0, 0, 30);
+        private static Timer m_Timer;
+        private static int m_TimerSync;
+
+        private static IDictionary<string, MetaEventStatus> m_EventStatus;
+        private static IList<EventSpawnData> m_Data;
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Loading current event status . . . ");
+            m_EventStatus = new EventTimerDataRequest().Execute().Events.ToDictionary(ev => ev.Id);
+            m_Data = new List<EventSpawnData>();
+            Console.WriteLine("Done.");
+
+            // start the timer
+            m_TimerSync = 0;
+            m_Timer = new Timer(m_PollRate.TotalMilliseconds);
+            m_Timer.Elapsed += WorkerThread;
+            m_Timer.Start();
+
+            Console.WriteLine("Beginning window analysis. Press any key to cease gathering data.");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            Console.Write("Halting window analysis . . . ");
+
+            // stop timer
+            m_Timer.Stop();
+
+            // wait for any existing threads to complete
+            SpinWait.SpinUntil(() => Interlocked.CompareExchange(ref m_TimerSync, -1, 0) == 0);
+
+            Console.WriteLine("Done.");
+
+            Console.Write("Writing data to file [data.csv] . . . ");
+            StreamWriter sw = new StreamWriter("data.csv");
+            sw.WriteLine("ev_id, spawn_time");
+            foreach (EventSpawnData data in m_Data)
+                sw.WriteLine("{0}, {1}", data.ev_id, data.spawn_time.ToString("G"));
+            sw.Close();
+        }
+
+        public static void WorkerThread(object sender, ElapsedEventArgs e)
+        {
+            // attempt to set the sync, if another of us is running, just exit
+            if (Interlocked.CompareExchange(ref m_TimerSync, 1, 0) != 0)
+                return;
+
+            // wrap in a try-catch so we can release our interlock if something fails
+            try
+            {
+                EventTimerDataResponse response = new EventTimerDataRequest().Execute();
+
+                foreach (MetaEventStatus ev in response.Events)
+                {
+                    if (!m_EventStatus.ContainsKey(ev.Id))
+                        m_EventStatus[ev.Id] = ev;
+                    else
+                    {
+                        MetaEventStatus oldEv = m_EventStatus[ev.Id];
+
+                        if (oldEv.StageTypeEnum == MetaEventStage.StageType.Invalid &&
+                            ev.StageTypeEnum != MetaEventStage.StageType.Invalid)
+                        {
+                            DateTime epoch = new DateTime(1970, 1, 1);
+                            DateTime oldTIme = epoch.AddMilliseconds((double)oldEv.Timestamp);
+                            DateTime newTIme = epoch.AddMilliseconds((double)ev.Timestamp);
+
+                            EventSpawnData spawnData = new EventSpawnData()
+                                {
+                                    ev_id = ev.Id,
+                                    spawn_time = newTIme - oldTIme
+                                };
+
+                            m_Data.Add(spawnData);
+                        }
+
+                        m_EventStatus[ev.Id] = ev;
+                    }
+                }
+            }
+            catch (Exception)
+            { }
+
+            // reset sync to 0
+            Interlocked.Exchange(ref m_TimerSync, 0);
+        }
+
+        public struct EventSpawnData
+        {
+            public string ev_id;
+            public TimeSpan spawn_time;
+        }
+    }
+}
---
+++ GuildWars2.TradingPost.Test/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+using GuildWars2.TradingPost;
+using HtmlAgilityPack;
+using RestSharp;
+
+namespace GuildWars2.TradingPost.Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string html;
+            SessionManager sm = SessionManager.GetInstance();
+            sm.Email = ""; // set email before running
+            sm.Password = ""; // set password before running
+
+            RequestUtil request = RequestUtil.NewInstance("https://leaderboards.guildwars2.com", "/en/na/achievements/guild/Guíld%20Of%20Dívíne%20Soldíers");
+            request.SetMethod(Method.GET);
+            request.SetCookie("s", sm.Session.Key);
+            IRestResponse r = request.Execute();
+            html = r.Content;
+
+            List<user> l = ParseUserPage(html);
+
+            TextArea.FontFamily = new FontFamily("Courier New");
+            TextArea.Text = string.Format("{0,-20} | {1,-6} | {2,-20} | {3}\n{4}", "User", "AP", "World", "Last Earned AP",
+                string.Join("\n", l.Select(u => string.Format("{0,-20} | {1,6} | {2,-20} | {3}", u.account, u.ap, u.world, u.lastapgain))));
+        }
+
+        private List<user> ParseUserPage(string html)
+        {
+            List<user> list = new List<user>();
+
+            HtmlDocument docNav = new HtmlDocument();
+            docNav.LoadHtml(html);
+
+            XPathNavigator nav = docNav.CreateNavigator();
+
+            XPathNodeIterator i = nav.Select("//table[contains(@class, 'achievements') and contains(@class, 'real')]/tbody/tr");
+
+            while (i.MoveNext())
+            {
+                user u = new user();
+
+                XPathNavigator node = i.Current.SelectSingleNode("./td[contains(@class, 'name')]");
+                if (node != null) u.account = node.Value.Trim();
+                
+                node = i.Current.SelectSingleNode("./td[contains(@class, 'achievements')]/span/span[not(contains(@class, 'additional'))]");
+                if (node != null) int.TryParse(node.Value.Trim(), out u.ap);
+                
+                node = i.Current.SelectSingleNode("./td[contains(@class, 'achievements')]/span/span[contains(@class, 'additional')]");
+                if (node != null) u.lastapgain = node.Value.Remove(0, node.Value.IndexOf("Since") + 5).Replace("&#x2F;", "/").Trim();
+                
+                node = i.Current.SelectSingleNode("./td[contains(@class, 'world')]");
+                if (node != null) u.world = node.Value.Trim();
+                
+                list.Add(u);
+            }
+
+            return list;
+        }
+
+        private struct user
+        {
+            public string account;
+            public int ap;
+            public string lastapgain;
+            public string world;
+        }
+    }
+}
