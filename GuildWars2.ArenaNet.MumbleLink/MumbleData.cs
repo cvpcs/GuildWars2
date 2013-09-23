adf76557386ad328d7c484d9eb3b2574ff60acde
Austen Dicken <cvpcsm@gmail.com>
2013-09-23 14:50:04 -0500

    Add Mumble link program for player positional data.

---
+++ GuildWars2.ArenaNet.MumbleLink/MumbleData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace GuildWars2.ArenaNet.MumbleLink
+{
+    [DataContract]
+    public class MumbleData
+    {
+        [DataMember(Name = "data_available")]
+        public bool DataAvaliable { get; set; }
+
+        [DataMember(Name = "game_name")]
+        public string GameName { get; set; }
+
+        [DataMember(Name = "player_name")]
+        public string PlayerName { get; set; }
+
+        [DataMember(Name = "server")]
+        public int Server { get; set; }
+
+        [DataMember(Name = "map")]
+        public int Map { get; set; }
+
+        [DataMember(Name = "pos_x")]
+        public float PositionX { get; set; }
+
+        [DataMember(Name = "pos_y")]
+        public float PositionY { get; set; }
+
+        [DataMember(Name = "pos_z")]
+        public float PositionZ { get; set; }
+
+        [DataMember(Name = "rot_player")]
+        public int RotationPlayer { get; set; }
+
+        [DataMember(Name = "rot_camera")]
+        public int RotationCamera { get; set; }
+    }
+}
---
+++ GuildWars2.ArenaNet.MumbleLink/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Threading;
+
+namespace GuildWars2.ArenaNet.MumbleLink
+{
+    public class Program
+    {
+        private static readonly MumbleLink MUMBLE = new MumbleLink();
+        private static readonly DataContractJsonSerializer SERIALIZER = new DataContractJsonSerializer(typeof(MumbleData));
+        private static readonly HttpJsonServer JSON_SERVER = new HttpJsonServer(38139);
+
+        public static void Main(string[] args)
+        {
+            JSON_SERVER.OnRequestReceived += GetJson;
+            JSON_SERVER.Start();
+
+            Console.WriteLine("Player position server is running. Press any key to exit this program.");
+            SpinWait.SpinUntil(() => Console.KeyAvailable);
+
+            JSON_SERVER.Stop();
+        }
+
+        private static string GetJson()
+        {
+            bool lockTaken = false;
+            string data = string.Empty;
+            MumbleData linkData = new MumbleData();
+
+            try
+            {
+                if (linkData.DataAvaliable = MUMBLE.DataAvailable)
+                {
+                    linkData.GameName = MUMBLE.GameName;
+                    linkData.PlayerName = MUMBLE.PlayerName;
+                    linkData.Server = MUMBLE.Server;
+                    linkData.Map = MUMBLE.Map;
+                    linkData.PositionX = MUMBLE.PositionX;
+                    linkData.PositionY = MUMBLE.PositionY;
+                    linkData.PositionZ = MUMBLE.PositionZ;
+                    linkData.RotationPlayer = MUMBLE.RotationPlayer;
+                    linkData.RotationCamera = MUMBLE.RotationCamera;
+                }
+
+                MemoryStream stream = new MemoryStream();
+                SERIALIZER.WriteObject(stream, linkData);
+                stream.Flush();
+                stream.Position = 0;
+                StreamReader reader = new StreamReader(stream);
+                data = reader.ReadToEnd();
+                reader.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return data;
+        }
+    }
+}
