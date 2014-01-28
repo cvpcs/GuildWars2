d844f502b32cc2c0bf6f376f91a7b2638de2e512
Austen Dicken <cvpcsm@gmail.com>
2014-01-28 14:32:43 -0600

    Fix test program after JSON deserializer update

--- GuildWars2.ArenaNet.Test/Program.cs
+++ GuildWars2.ArenaNet.Test/Program.cs
@@ -14,19 +14,19 @@ namespace GuildWars2.ArenaNet.Test
     {
         public static void Main(string[] args)
         {
-            IDictionary<string, Event> event_names = new EventNamesRequest().Execute().Where(e => {
+            IDictionary<Guid, Event> event_names = new EventNamesRequest().Execute().Where(e => {
                 string n = e.Name.ToLower();
 
                 return (n.Contains("scarlet") || n.Contains("aetherblade") || n.Contains("molten"));
-            }).ToDictionary(e => e.Id.ToString().ToUpper());
+            }).ToDictionary(e => e.Id);
             IDictionary<int, Map> map_names = new MapNamesRequest().Execute().ToDictionary(m => m.Id);
-            IDictionary<string, EventDetails> event_details = new EventDetailsRequest().Execute().Events;
+            IDictionary<Guid, EventDetails> event_details = new EventDetailsRequest().Execute().Events;
 
-            IDictionary<int, IList<string>> map_events = new Dictionary<int, IList<string>>();
+            IDictionary<int, IList<Guid>> map_events = new Dictionary<int, IList<Guid>>();
 
             StreamWriter s = new StreamWriter(new FileStream(@"D:\tmp\stuff.txt", FileMode.Create));
             
-            foreach (string eid in event_names.Keys)
+            foreach (Guid eid in event_names.Keys)
             {
                 if (!event_details.ContainsKey(eid))
                     continue;
@@ -34,7 +34,7 @@ namespace GuildWars2.ArenaNet.Test
                 EventDetails details = event_details[eid];
 
                 if (!map_events.ContainsKey(details.MapId))
-                    map_events[details.MapId] = new List<string>();
+                    map_events[details.MapId] = new List<Guid>();
 
                 map_events[details.MapId].Add(eid);
             }
@@ -43,7 +43,7 @@ namespace GuildWars2.ArenaNet.Test
             {
                 s.WriteLine(string.Format("Scarlet events in [{0}]:", map_names[mapid].Name));
 
-                foreach (string eid in map_events[mapid])
+                foreach (Guid eid in map_events[mapid])
                 {
                     s.WriteLine(string.Format("  [{0}]: [{1}]", eid, event_names[eid].Name));
                 }
