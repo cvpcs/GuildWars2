39c03d37327e5a14a26be310e6fcc4ecf8c88172
Austen Dicken <cvpcsm@gmail.com>
2018-03-28 19:40:00 -0500

    Move overlay window UI into a control library so it can be used in standard XAML projects

--- GuildWars2.Overlay.Controls/WindowUI.xaml.cs
+++ GuildWars2.Overlay.Controls/WindowUI.xaml.cs
@@ -5,7 +5,7 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Interop;
 
-namespace GuildWars2.Overlay
+namespace GuildWars2.Overlay.Controls
 {
     /// <summary>
     /// Interaction logic for WindowUI.xaml
