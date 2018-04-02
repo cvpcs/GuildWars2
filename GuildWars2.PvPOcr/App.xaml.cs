68612b9276ebaba608b8d698d3f1d950012d2f19
Austen Dicken <cvpcsm@gmail.com>
2018-04-02 13:49:09 -0500

    Log unhandled exceptions to err.txt in case of problems.

--- GuildWars2.PvPOcr/App.xaml.cs
+++ GuildWars2.PvPOcr/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
 
 namespace GuildWars2.PvPOcr
 {
@@ -7,5 +9,20 @@ namespace GuildWars2.PvPOcr
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) => this.LogUnhandledException(e.ExceptionObject as Exception);
+            this.DispatcherUnhandledException += (s, e) => this.LogUnhandledException(e.Exception);
+
+            base.OnStartup(args);
+        }
+
+        private void LogUnhandledException(Exception e)
+        {
+            if (e != null)
+            {
+                File.WriteAllText("./err.txt", e.ToString());
+            }
+        }
     }
 }
