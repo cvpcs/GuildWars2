f4f172efcbe0ca2bbf502f0cc19e7344a3ef6a70
Austen Dicken <cvpcsm@gmail.com>
2018-04-16 22:52:42 -0500

    Add some form of configuration management

---
+++ GuildWars2.PvPCasterToolbox/Configuration/AppConfig.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+using LibHotKeys;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+
+namespace GuildWars2.PvPCasterToolbox.Configuration
+{
+    // TODO: can this be done with IConfiguration or IOptions?
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+    public class AppConfig : INotifyPropertyChanged
+    {
+        private static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented,
+            Converters = new List<JsonConverter> { new HotKeyConverter(), new StringEnumConverter() }
+        };
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region Backing properties / defaults
+        private bool useOverlayMode = true;
+        private bool useLiveSetup = true;
+        private Rectangle redSection = new Rectangle((int)(0.42708 * SystemParameters.PrimaryScreenWidth),
+                                                     (int)(0.00000 * SystemParameters.PrimaryScreenHeight),
+                                                     (int)(0.04167 * SystemParameters.PrimaryScreenWidth),
+                                                     (int)(0.03704 * SystemParameters.PrimaryScreenHeight));
+        private Rectangle blueSection = new Rectangle((int)(0.53125 * SystemParameters.PrimaryScreenWidth),
+                                                      (int)(0.00000 * SystemParameters.PrimaryScreenHeight),
+                                                      (int)(0.04167 * SystemParameters.PrimaryScreenWidth),
+                                                      (int)(0.03704 * SystemParameters.PrimaryScreenHeight));
+        private RectangleF redScoreBarPosition;
+        private RectangleF blueScoreBarPosition;
+        private ImageModulationParameters redScoreBarModulation;
+        private ImageModulationParameters blueScoreBarModulation;
+        private HotKey screenshotHotKey;
+        #endregion
+
+        #region Serialized properties
+        [JsonProperty]
+        public bool UseOverlayMode
+        {
+            get => this.useOverlayMode;
+            set => this.SetValue(ref this.useOverlayMode, value);
+        }
+
+        [JsonProperty]
+        public bool UseLiveSetup
+        {
+            get => this.useLiveSetup;
+            set => this.SetValue(ref this.useLiveSetup, value);
+        }
+
+        [JsonProperty]
+        public Rectangle RedSection
+        {
+            get => this.redSection;
+            set => this.SetValue(ref this.redSection, value);
+        }
+
+        [JsonProperty]
+        public Rectangle BlueSection
+        {
+            get => this.blueSection;
+            set => this.SetValue(ref this.blueSection, value);
+        }
+
+        [JsonProperty]
+        public RectangleF RedScoreBarPosition
+        {
+            get => this.redScoreBarPosition;
+            set => this.SetValue(ref this.redScoreBarPosition, value);
+        }
+
+        [JsonProperty]
+        public RectangleF BlueScoreBarPosition
+        {
+            get => this.blueScoreBarPosition;
+            set => this.SetValue(ref this.blueScoreBarPosition, value);
+        }
+
+        [JsonProperty]
+        public ImageModulationParameters RedScoreBarModulation
+        {
+            get => this.redScoreBarModulation;
+            set => this.SetValue(ref this.redScoreBarModulation, value);
+        }
+
+        [JsonProperty]
+        public ImageModulationParameters BlueScoreBarModulation
+        {
+            get => this.blueScoreBarModulation;
+            set => this.SetValue(ref this.blueScoreBarModulation, value);
+        }
+
+        [JsonProperty]
+        public HotKey ScreenshotHotKey
+        {
+            get => this.screenshotHotKey;
+            set => this.SetValue(ref this.screenshotHotKey, value);
+        }
+        #endregion
+
+        public void Load(string path)
+            => JsonConvert.PopulateObject(File.ReadAllText(path), this, JsonSerializerSettings);
+
+        public void Save(string path)
+            => File.WriteAllText(path, JsonConvert.SerializeObject(this, JsonSerializerSettings));
+
+        private void SetValue<TValue>(ref TValue property, TValue value, [CallerMemberName]string propertyName = null)
+        {
+            if (!EqualityComparer<TValue>.Default.Equals(property, value))
+            {
+                property = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private class HotKeyConverter : JsonConverter<HotKey>
+        {
+            public override bool CanWrite => false;
+
+            public override HotKey ReadJson(JsonReader reader, Type objectType, HotKey existingValue, bool hasExistingValue, JsonSerializer serializer)
+            {
+                JObject jo = JObject.Load(reader);
+                return new HotKey(jo[nameof(HotKey.Key)].ToObject<Key>(),
+                                  jo[nameof(HotKey.Modifiers)].ToObject<ModifierKeys>());
+            }
+
+            public override void WriteJson(JsonWriter writer, HotKey value, JsonSerializer serializer)
+                => throw new NotImplementedException();
+        }
+    }
+}
--- GuildWars2.PvPCasterToolbox/MainWindow.xaml.cs
+++ GuildWars2.PvPCasterToolbox/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Windows;
+using GuildWars2.PvPCasterToolbox.Configuration;
 using GuildWars2.PvPCasterToolbox.TabPages;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Win32;
 
 namespace GuildWars2.PvPCasterToolbox
 {
@@ -10,12 +13,79 @@ namespace GuildWars2.PvPCasterToolbox
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow(IServiceProvider serviceProvider)
+        private const string DefaultExt = ".json";
+        private const string Filter = "JSON Files (*.json)|*.json";
+
+        private AppConfig appConfig;
+        private ILogger logger;
+
+        public MainWindow(IServiceProvider serviceProvider, AppConfig appConfig, ILogger<MainWindow> logger)
         {
+            this.appConfig = appConfig;
+            this.logger = logger;
+
             InitializeComponent();
 
             Pages.Items.Add(ActivatorUtilities.GetServiceOrCreateInstance<GameTracking>(serviceProvider));
             Pages.Items.Add(ActivatorUtilities.GetServiceOrCreateInstance<ConsoleLog>(serviceProvider));
         }
+
+        // TODO: use ICommand model?
+        public void LoadConfiguration_Clicked(object sender, EventArgs args)
+        {
+            var dialog = new OpenFileDialog
+            {
+                DefaultExt = DefaultExt,
+                Filter = Filter,
+                Multiselect = false,
+                Title = "Load configuration"
+            };
+
+            bool? result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                try
+                {
+                    this.appConfig.Load(dialog.FileName);
+                    this.logger.LogInformation($"Loaded configuration from {dialog.FileName}");
+                }
+                catch (Exception e)
+                {
+                    this.logger.LogError(e, string.Empty);
+                }
+            }
+        }
+
+        // TODO: use ICommand model?
+        public void SaveConfiguration_Clicked(object sender, EventArgs args)
+        {
+            var dialog = new SaveFileDialog
+            {
+                AddExtension = true,
+                DefaultExt = DefaultExt,
+                Filter = Filter,
+                Title = "Save configuration"
+            };
+
+            bool? result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                try
+                {
+                    this.appConfig.Save(dialog.FileName);
+                    this.logger.LogInformation($"Saved configuration to {dialog.FileName}");
+                }
+                catch (Exception e)
+                {
+                    this.logger.LogError(e, string.Empty);
+                }
+            }
+        }
+
+        // TODO: use ICommand model?
+        public void Exit_Clicked(object sender, EventArgs args)
+            => this.Close();
     }
 }
