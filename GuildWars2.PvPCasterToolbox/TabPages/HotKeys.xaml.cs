9d121ad6bf9d0e612df14a414493bf5464a05e4f
Austen Dicken <cvpcsm@gmail.com>
2018-04-18 22:45:20 -0500

    Add hotkey support

---
+++ GuildWars2.PvPCasterToolbox/TabPages/HotKeys.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using GuildWars2.PvPCasterToolbox.Configuration;
+using LibHotKeys;
+using Microsoft.Extensions.Logging;
+
+namespace GuildWars2.PvPCasterToolbox.TabPages
+{
+    public partial class HotKeys : TabItem, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string ScreenshotHotKey => this.appConfig.ScreenshotHotKey?.ToString();
+
+        private object screenshotHotKeyLock = new object();
+        private int? screenshotHotKeyId = null;
+
+        private int screenshotCaptureState = (int)CaptureState.Idle;
+
+        private AppConfig appConfig;
+        private ILogger logger;
+
+        public HotKeys(Gw2ScreenshotProcessor gw2ScreenshotProcessor,
+                       AppConfig appConfig,
+                       ILogger<HotKeys> logger)
+        {
+            this.appConfig = appConfig;
+            this.logger = logger;
+
+            if (this.appConfig.ScreenshotHotKey != null)
+            {
+                this.SetScreenshotHotKey(this.appConfig.ScreenshotHotKey);
+            }
+
+            InitializeComponent();
+
+            this.appConfig.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == nameof(AppConfig.ScreenshotHotKey))
+                {
+                    this.SetScreenshotHotKey(this.appConfig.ScreenshotHotKey);
+                    this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ScreenshotHotKey)));
+                }
+            };
+
+            HotKeyManager.HotKeyPressed += hotKey =>
+            {
+                if (hotKey.Equals(this.appConfig.ScreenshotHotKey))
+                {
+                    // set
+                    Interlocked.CompareExchange(ref screenshotCaptureState, (int)CaptureState.Pending, (int)CaptureState.Idle);
+                }
+            };
+
+            gw2ScreenshotProcessor.ScreenshotCaptured += screenshot =>
+            {
+                if (Interlocked.CompareExchange(ref screenshotCaptureState, (int)CaptureState.Active, (int)CaptureState.Pending) == (int)CaptureState.Pending)
+                {
+                    // TODO: this was added to allow for multiple screenshots for OBS to update to reduce lag.
+                    //       consider a better way to support this
+                    const int numScreenshotCopies = 4;
+                    for (var i = 0; i < numScreenshotCopies; i++)
+                    {
+                        var screenshotFile = $"screenshot{i:00}.png";
+                        screenshot.Save($"./{screenshotFile}", ImageFormat.Png);
+                        this.logger.LogInformation($"Saved screenshots to {screenshotFile}");
+                    }
+
+                    Interlocked.Exchange(ref screenshotCaptureState, (int)CaptureState.Idle);
+                }
+            };
+        }
+
+        // TODO: ICommand?
+        public void ChangeScreenshotHotKey_Clicked(object sender, EventArgs args)
+        {
+            var messageBox = new Window
+            {
+                Content = new TextBlock
+                {
+                    Text = "Press any key combination to bind it to the screenshot hotkey.\n"
+                         + "Press ESC to cancel or DEL to clear.",
+                    TextAlignment = TextAlignment.Center,
+                    TextWrapping = TextWrapping.Wrap
+                },
+                Owner = Window.GetWindow(this),
+                Width = 250,
+                Height = 80,
+                Topmost = true,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                WindowStyle = WindowStyle.None
+            };
+
+            messageBox.KeyDown += (s, e) =>
+            {
+                e.Handled = true;
+                switch (e.Key == Key.System ? e.SystemKey : e.Key)
+                {
+                    case Key.LeftCtrl:
+                    case Key.RightCtrl:
+                    case Key.LeftShift:
+                    case Key.RightShift:
+                    case Key.LeftAlt:
+                    case Key.RightAlt:
+                    case Key.LWin:
+                    case Key.RWin:
+                        return;
+
+                    case Key.Escape:
+                        messageBox.Close();
+                        break;
+
+                    case Key.Delete:
+                        this.SetScreenshotHotKey(null);
+                        messageBox.Close();
+                        break;
+
+                    default:
+                        this.SetScreenshotHotKey(new HotKey(e.Key, e.KeyboardDevice.Modifiers));
+                        messageBox.Close();
+                        break;
+                }
+            };
+
+            messageBox.Show();
+        }
+
+        private void SetScreenshotHotKey(HotKey hotKey)
+        {
+            lock (screenshotHotKeyLock)
+            {
+                if (this.screenshotHotKeyId.HasValue)
+                {
+                    HotKeyManager.UnregisterHotKey(this.screenshotHotKeyId.Value);
+                }
+
+                this.screenshotHotKeyId = hotKey != null ? (int?)HotKeyManager.RegisterHotKey(hotKey) : null;
+                this.appConfig.ScreenshotHotKey = hotKey;
+            }
+        }
+
+        private enum CaptureState
+        {
+            Idle = 0, // idle
+            Pending = 1, // pending next screenshot
+            Active = 2 // currently capturing
+        }
+    }
+}
